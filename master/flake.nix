d06075808d6b34b3f68a45500a08b4ed9837fbed
hugosenari <hugosenari@gmail.com>
2022-03-12 03:23:50 -0300

    chore: re index metatools

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Metaprogramming tools for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-metatools-master.flake = false;
+  inputs.src-metatools-master.owner = "jxy";
+  inputs.src-metatools-master.ref   = "refs/heads/master";
+  inputs.src-metatools-master.repo  = "metatools";
+  inputs.src-metatools-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-metatools-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-metatools-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_2_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Metaprogramming tools for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-metatools-v0_2_0.flake = false;
+  inputs.src-metatools-v0_2_0.owner = "jxy";
+  inputs.src-metatools-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-metatools-v0_2_0.repo  = "metatools";
+  inputs.src-metatools-v0_2_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-metatools-v0_2_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-metatools-v0_2_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
