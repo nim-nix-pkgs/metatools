8c249d8cc79d090617e2c1503e5531bb23c93e66
hugosenari <hugosenari@gmail.com>
2022-09-03 19:56:51 -0300

    feat(nimpkgs): update lock

--- source/test/config.nims
+++
@@ -1,2 +0,0 @@
---path:".."
---nimcache:nimcache
--- source/test/tinline013.nim
+++
@@ -1,8 +0,0 @@
-import metatools
-
-echo "* static[T]"
-proc fs(x:int, y:static[int]):int = x*y
-inlineProcs:
-  var x = 2
-  let y = x.fs 3
-  echo y
